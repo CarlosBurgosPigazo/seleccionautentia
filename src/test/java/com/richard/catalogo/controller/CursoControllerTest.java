019ebff060c0367b4cc5226f0d4561ec3db2928c
remusRD <remusRichardDumitrache@gmail.com>
2018-07-02 12:19:35 +0200

    modified travis.yml

--- src/test/java/com/richard/catalogo/controller/CursoControllerTest.java
+++ src/test/java/com/richard/catalogo/controller/CursoControllerTest.java
@@ -1,13 +1,21 @@
 package com.richard.catalogo.controller;
 
 import com.richard.catalogo.domain.Curso;
+import com.richard.catalogo.domain.Temario;
 import com.richard.catalogo.service.CursoService;
 import org.junit.Before;
 import org.junit.Test;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+import java.io.File;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
-
+import static org.mockito.Mockito.when;
 
 
 public class CursoControllerTest {
@@ -17,6 +25,7 @@ public class CursoControllerTest {
     @Before
     public void setUp() throws Exception {
         sut.setCursoService(cursoService);
+        sut.init();
     }
     @Test
     public void alInsertarUnCursoElCursoAInsertarSeDebeResetear(){
@@ -32,5 +41,21 @@ public class CursoControllerTest {
 
         assertEquals(cursoEsperado, sut.getCursoAInsertar());
 
+    }
+    @Test
+    public void alSubirUnArchivoElTemarioActualDebeActualizarseCorrectamente(){
+        FileUploadEvent evento = mock(FileUploadEvent.class);
+        UploadedFile fichero = mock(UploadedFile.class);
+        when(evento.getFile()).thenReturn(fichero);
+        when(fichero.getContentType()).thenReturn("text/plain");
+        when(fichero.getContents()).thenReturn("test".getBytes());
+        when(fichero.getFileName()).thenReturn("test.txt");
+
+        sut.uploadTemarioEvent(evento);
+
+        Temario temarioAsociado = sut.getTemarioAsociado();
+        assertEquals(temarioAsociado.getExtension(), "text/plain");
+        assertTrue(Arrays.equals(temarioAsociado.getBytes(), "test".getBytes()));
+        assertEquals(temarioAsociado.getNombre(), "test.txt");
     }
 }
